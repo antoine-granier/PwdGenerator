3859430d0a8ef6f764e6fa5376ff4c76c4fb51e9
antoine-granier <antoinegr93@gmail.com>
2022-02-02 16:24:41 +0100

    fix

--- src/main/java/com/pwdgenerator/PwdGenerator.java
+++ src/main/java/com/pwdgenerator/PwdGenerator.java
@@ -1,9 +1,7 @@
 package com.pwdgenerator;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,6 +12,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+/*
+    ____               ________                           __
+   / __ \_      ______/ / ____/__  ____  ___  _________ _/ /_____  _____
+  / /_/ / | /| / / __  / / __/ _ \/ __ \/ _ \/ ___/ __ `/ __/ __ \/ ___/
+ / ____/| |/ |/ / /_/ / /_/ /  __/ / / /  __/ /  / /_/ / /_/ /_/ / /
+/_/     |__/|__/\__,_/\____/\___/_/ /_/\___/_/   \__,_/\__/\____/_/
+
+
+ */
 public class PwdGenerator extends Application {
     @Override
     public void start(Stage stage) throws IOException, URISyntaxException {
